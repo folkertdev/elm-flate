a9227e073907133c93630d844f8546869e6b96a1
Folkert de Vries <folkert@folkertdev.nl>
2019-10-30 15:55:32 +0100

    narrow down test case

--- tests/Issue2.elm
+++ tests/Issue2.elm
@@ -34,11 +34,22 @@ suite =
                     |> Expect.equal (Just (List.length Issue2Data.expected))
         , test "first bytes" <|
             \_ ->
-                Issue2Data.input
-                    |> Flate.inflateGZip
-                    |> Maybe.andThen (\b -> Decode.decode (unsignedInt8List (Bytes.width b)) b)
-                    |> Maybe.map (List.take 100)
-                    |> Expect.equal (Just (List.take 100 Issue2Data.expected))
+                case Issue2Data.input |> Flate.inflateGZip |> Maybe.andThen (\b -> Decode.decode (unsignedInt8List (Bytes.width b)) b) of
+                    Just v ->
+                        let
+                            size =
+                                List.length v
+
+                            n =
+                                900
+                        in
+                        List.drop (size - n) v
+                            |> List.take 100
+                            |> Expect.equal (List.take 100 <| List.drop (size - n) Issue2Data.expected)
+
+                    Nothing ->
+                        Just []
+                            |> Expect.equal (Just (List.take 100 <| List.drop 0 Issue2Data.expected))
         ]
 
 
