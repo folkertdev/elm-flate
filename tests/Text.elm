4d661c4434ba25169d9ff1a67cbaf3677134a585
Folkert <folkert@folkertdev.nl>
2019-10-30 22:18:22 +0100

    move lz77 tests over to array

---
+++ tests/Text.elm
@@ -0,0 +1,5 @@
+module Text exposing (text)
+
+
+text =
+    "write a few lines in haste to say that I am safe-and well advanced on my voyage. This letter will reach England by a merchantman now on its homeward voyage from Archangel; more fortunate than I, who may not see my native land, perhaps, for many years. I am, however, in good spirits"
