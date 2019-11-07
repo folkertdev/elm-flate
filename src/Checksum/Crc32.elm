e0e4a2ff0bdc59d502f2845f66252cc02e607850
Folkert <folkert@folkertdev.nl>
2019-11-08 00:14:18 +0100

    3X speed increase for crc

--- src/Checksum/Crc32.elm
+++ src/Checksum/Crc32.elm
@@ -29,69 +29,140 @@ tinf_crc32 buffer =
 
         initialCrc =
             0xFFFFFFFF
-
-        -- Because of the bitwise and, this is actually completely safe
-        unsafeGet i arr =
-            case Array.get i arr of
-                Nothing ->
-                    0
-
-                Just v ->
-                    v
     in
     if length == 0 then
         0
 
     else
-        let
-            go ( i, crc ) =
-                if i < length then
-                    Decode.unsignedInt8
-                        |> Decode.map
-                            (\byte ->
-                                let
-                                    a =
-                                        Bitwise.xor crc byte
-                                            |> Bitwise.shiftRightZfBy 0
-
-                                    b =
-                                        unsafeGet (Bitwise.and a 0x0F) tinf_crc32tab
-                                            |> Bitwise.xor (Bitwise.shiftRightZfBy 4 a)
-                                            |> Bitwise.shiftRightZfBy 0
-
-                                    c =
-                                        unsafeGet (Bitwise.and b 0x0F) tinf_crc32tab
-                                            |> Bitwise.xor (Bitwise.shiftRightZfBy 4 b)
-                                in
-                                Decode.Loop ( i + 1, c )
-                            )
-
-                else
-                    Bitwise.xor crc 0xFFFFFFFF
-                        |> Bitwise.shiftRightZfBy 0
-                        |> Decode.Done
-                        |> Decode.succeed
-        in
-        Decode.decode (Decode.loop ( 0, initialCrc ) go) buffer
+        Decode.decode (Decode.loop { remaining = length, crc = initialCrc } crc32Help) buffer
             |> Maybe.withDefault 0
 
 
-tinf_crc32tab =
-    Array.fromList
-        [ 0x00
-        , 0x1DB71064
-        , 0x3B6E20C8
-        , 0x26D930AC
-        , 0x76DC4190
-        , 0x6B6B51F4
-        , 0x4DB26158
-        , 0x5005713C
-        , 0xEDB88320
-        , 0xF00F9344
-        , 0xD6D6A3E8
-        , 0xCB61B38C
-        , 0x9B64C2B0
-        , 0x86D3D2D4
-        , 0xA00AE278
-        , 0xBDBDF21C
-        ]
+crc32Help { remaining, crc } =
+    if remaining >= 8 then
+        Decode.map2
+            (\word1 word2 ->
+                let
+                    byte1 =
+                        Bitwise.shiftRightZfBy 24 word1 |> Bitwise.and 0xFF
+
+                    byte2 =
+                        Bitwise.shiftRightZfBy 16 word1 |> Bitwise.and 0xFF
+
+                    byte3 =
+                        Bitwise.shiftRightZfBy 8 word1 |> Bitwise.and 0xFF
+
+                    byte4 =
+                        Bitwise.and 0xFF word1
+
+                    byte5 =
+                        Bitwise.shiftRightZfBy 24 word2 |> Bitwise.and 0xFF
+
+                    byte6 =
+                        Bitwise.shiftRightZfBy 16 word2 |> Bitwise.and 0xFF
+
+                    byte7 =
+                        Bitwise.shiftRightZfBy 8 word2 |> Bitwise.and 0xFF
+
+                    byte8 =
+                        Bitwise.and 0xFF word2
+                in
+                Decode.Loop
+                    { remaining = remaining - 8
+                    , crc =
+                        -- potential speedup: inline these calls
+                        crc
+                            |> step byte1
+                            |> step byte2
+                            |> step byte3
+                            |> step byte4
+                            |> step byte5
+                            |> step byte6
+                            |> step byte7
+                            |> step byte8
+                    }
+            )
+            (Decode.unsignedInt32 Bytes.BE)
+            (Decode.unsignedInt32 Bytes.BE)
+
+    else if remaining > 0 then
+        Decode.unsignedInt8
+            |> Decode.map
+                (\byte ->
+                    Decode.Loop { remaining = remaining - 1, crc = step byte crc }
+                )
+
+    else
+        Bitwise.xor crc 0xFFFFFFFF
+            |> Bitwise.shiftRightZfBy 0
+            |> Decode.Done
+            |> Decode.succeed
+
+
+step byte crc =
+    let
+        a =
+            Bitwise.xor crc byte
+                |> Bitwise.shiftRightZfBy 0
+
+        b =
+            tinf_crc32case (Bitwise.and a 0x0F)
+                |> Bitwise.xor (Bitwise.shiftRightZfBy 4 a)
+                |> Bitwise.shiftRightZfBy 0
+
+        c =
+            tinf_crc32case (Bitwise.and b 0x0F)
+                |> Bitwise.xor (Bitwise.shiftRightZfBy 4 b)
+    in
+    c
+
+
+tinf_crc32case i =
+    case i of
+        0 ->
+            0x00
+
+        1 ->
+            0x1DB71064
+
+        2 ->
+            0x3B6E20C8
+
+        3 ->
+            0x26D930AC
+
+        4 ->
+            0x76DC4190
+
+        5 ->
+            0x6B6B51F4
+
+        6 ->
+            0x4DB26158
+
+        7 ->
+            0x5005713C
+
+        8 ->
+            0xEDB88320
+
+        9 ->
+            0xF00F9344
+
+        10 ->
+            0xD6D6A3E8
+
+        11 ->
+            0xCB61B38C
+
+        12 ->
+            0x9B64C2B0
+
+        13 ->
+            0x86D3D2D4
+
+        14 ->
+            0xA00AE278
+
+        _ ->
+            0xBDBDF21C
